547f97180450b5a0afd60cfd3085c7e06e73fecd
aryxnmodi <aryancmodi2002@gmail.com>
2024-06-03 16:22:09 +0530

    hello

--- hospitalMS-main/resources/views/livewire/contactus.blade.php
+++ hospitalMS-main/resources/views/livewire/contactus.blade.php
@@ -45,13 +45,7 @@
                     </div>
                 </div>
             </form>
-            @if (session()->has('message'))
-                <script>
-                    alert(' {{ session('message') }}')
-
-                </script>
-            @endif
-        </div>
+            </div>
         <div id="googleMap" style="width:100%;height:450px;"></div>
     </div>
 </div>
