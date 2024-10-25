abd02f9aa4e6b68cbc19ff4fd8b43487236781f6
aryxnmodi <aryancmodi2002@gmail.com>
2024-10-25 11:17:16 +0530

    hello

--- Tops Vadodara/Task 25_10/Task1.js
+++ Tops Vadodara/Task 25_10/Task1.js
@@ -20,3 +20,16 @@
 //console.log(typeof(second_number));
 //let addition = first_number+second_number;
 //console.log(addition);
+
+
+//let age = 8252255522526526252625625263526325625326n;
+//console.log(typeof(age));
+
+//let name = "hello world";
+//console.log(typeof(name));
+
+//Area of circle : Pi *r*r;
+//let radius = parseFloat(promt("enter the radius of circle"));
+//let area_of_circle = Math.PI * Math.pow(radius,2);
+//console.log('area of circle is : ${area of circle}');
+//console.log(Math.PI);
