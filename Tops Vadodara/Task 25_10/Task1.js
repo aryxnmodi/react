14b28dd21368e115d635031476af4b9a57b5d2ff
aryxnmodi <aryancmodi2002@gmail.com>
2024-10-25 11:07:35 +0530

    hello

---
+++ Tops Vadodara/Task 25_10/Task1.js
@@ -0,0 +1,22 @@
+//let age = prompt("enter your age :");
+//console.log(age);
+
+//let first_number = promt("enter first number :");
+//let second_number = promt("enter second number :");
+
+//let addition = first_number + second_number;
+//console.log(addition);
+//let first_number = promt("enter first number :");
+//let second_number = promt("enter second number :");
+//console.log(typeof(first_number));
+//console.log(typeof(second_number));
+
+//let addition = first_number+second_number;
+//console.log(addition);
+
+//let first_number = parseInt(promt("enter first number :"));
+//let second_number = parseInt(promt("enter second number :"));
+//console.log(typeof(first_number));
+//console.log(typeof(second_number));
+//let addition = first_number+second_number;
+//console.log(addition);
