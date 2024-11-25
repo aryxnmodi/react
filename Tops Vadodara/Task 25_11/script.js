56ae4c4fd410151b2fcf56b779e8c4de127ba78c
aryxnmodi <aryancmodi2002@gmail.com>
2024-11-25 11:09:02 +0530

    hello

---
+++ Tops Vadodara/Task 25_11/script.js
@@ -0,0 +1,19 @@
+//function additionNumbers(x,y,z,p){
+  //  return x+y+z+p;
+
+//}
+//array : collection of element
+
+//const data = [2,3,4,5];
+//const result = additionNumbers(2,3,4);
+//console.log(`addition of numbers are : ${data}`);
+
+//function : map, filter, reduce
+
+const data = [2,3,4,5,6,7,8,9,12,13];
+//const result = data.map((x)=> x*2);
+//console.log(result);
+
+
+//const result = data.filter((x)=> x>5);
+//console.log(result);
