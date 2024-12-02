47123621135d746f3fedcdb2045598af41c13b6b
aryxnmodi <aryancmodi2002@gmail.com>
2024-12-02 11:16:59 +0530

    hello

---
+++ Tops Vadodara/Task 1_12/script.js
@@ -0,0 +1,20 @@
+//const mydata = [3,5,4,8,7,9,15];
+//console.log(typeof(mydata));
+
+//mydata.map((x)=>{
+//    console.log(x);
+//});
+
+//const result = mydata.map((x)=> x>20);
+//console.log(result);
+
+//mydata.filter((x)=>{
+//    console.log(x);
+//});
+
+//const result = mydata.filter((x)=> x>10);
+//console.log(result);
+
+const mydata = [3,5,4,8,7,9,15];
+const result = mydata.filter((x)=> x*10);
+console.log(result);
