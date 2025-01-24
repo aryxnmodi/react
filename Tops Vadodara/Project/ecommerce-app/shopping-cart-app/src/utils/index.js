c79cd5ad7f0157c8466e0c4b90ec9740e0b01bf8
aryxnmodi <aryancmodi2002@gmail.com>
2025-01-24 09:50:07 +0530

    hello

--- Tops Vadodara/Project/ecommerce-app/shopping-cart-app/src/utils/index.js
+++
@@ -1,7 +0,0 @@
-import { colors } from './colors'
-import { fontStyles } from './fontStyles'
-import { textStyles } from './textStyles'
-import { scale, verticalScale } from './scaling'
-import { alignment } from './alignment'
-
-export { colors, alignment, fontStyles, textStyles, scale, verticalScale }
